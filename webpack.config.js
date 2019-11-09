8dde4194c74e5393523c7bb64e4f9e66d35c1a6e
Ivan <belzee92@gmail.com>
2019-11-09 11:10:51 -0500

    update Prettier and ESlint rules

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'production', // "production" | "development" | "none"
   entry: './src/Rating.jsx',
   output: {
     path: path.resolve(__dirname, 'build'),
@@ -27,7 +26,6 @@ module.exports = {
       }
     ]
   },
-
   externals: {
     react: 'commonjs react'
   }
